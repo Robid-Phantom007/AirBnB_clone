7770d24d495ea76d8a4ac238ce4afc479025bcfd
Robid-Phantom007 <robidnomolos@gmail.com>
2022-10-30 14:40:34 -0700

    Task Updated

--- models/amenity.py
+++ models/amenity.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
-""" holds class Amenity"""
-import models
-from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+"""Defines the Amenity class."""
+from models.base_model import BaseModel
 
-class Amenity(BaseModel, Base):
-    """Representation of Amenity """
-    if models.storage_t == 'db':
-        _tablename_ = 'amenities'
-        name = Column(String(128) nullable=False)
-    else
-        name = ""
 
-    def_init_(self, args, *kwargs):
-        """initializes Amenity"""
-        super()._init_(args, *kwargs)
+class Amenity(BaseModel):
+    """Represent an amenity.
+
+    Attributes:
+        name (str): The name of the amenity.
+    """
+
+    name = ""
--- models/user.py
+++ models/user.py
@@ -1,47 +1,19 @@
 #!/usr/bin/python3
-""" holds class User"""
-import hashlib
-import models
-from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+"""Defines the User class."""
+from models.base_model import BaseModel
 
 
-class User(BaseModel, Base):
-    """Representation of a user """
-    if models.storage_t == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship(
-            "Place",
-            cascade="all, delete, delete-orphan",
-            backref="user"
-        )
-        reviews = relationship(
-            "Review",
-            cascade="all, delete, delete-orphan",
-            backref="user"
-        )
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+class User(BaseModel):
+    """Represent a User.
 
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
+    Attributes:
+        email (str): The email of the user.
+        password (str): The password of the user.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+    """
 
-    def __setattr__(self, __name: str, __value) -> None:
-        '''Sets an attribute of this class to a given value.'''
-        if __name == 'password':
-            if type(__value) is str:
-                m = hashlib.md5(bytes(__value, 'utf-8'))
-                super().__setattr__(__name, m.hexdigest())
-        else:
-            super().__setattr__(__name, __value)
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
